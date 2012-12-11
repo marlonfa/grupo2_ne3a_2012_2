33354bf9add4752152a4d202d3b79bceb6604f1c
marlonfa <marlonfa@gmail.com>
2012-12-10 22:41:33 -0200

    Exclusão dos comentários gerados automaticamente pelo NetBeans

--- Imobiliaria/src/imobiliaria/Imobiliaria.java
+++ Imobiliaria/src/imobiliaria/Imobiliaria.java
@@ -1,19 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package imobiliaria;
 
-/**
- *
- * @author marlon
- */
+
 public class Imobiliaria {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
 }
