a305f5919574e6939a6081603c2b371a2b644dbf
eder92esf <eder.esf92@gmail.com>
2013-02-16 13:40:41 -0200

    # 25 Criação da Classe DAO de Usuário

---
+++ Imobiliaria/src/dao/UsuarioDAO.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import entidade.usuario.UsuarioEntidade;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author eder
+ */
+public class UsuarioDAO {
+
+    public void adicionar(UsuarioEntidade usuario) {
+    }
+
+    public void alterar(UsuarioEntidade cliente) {
+    }
+
+    public List<UsuarioEntidade> consultar(String busca) {
+        List<UsuarioEntidade> lista = new ArrayList<>();
+        return lista;
+    }
+
+    public void remover(UsuarioEntidade usuario) {
+    }
+}
