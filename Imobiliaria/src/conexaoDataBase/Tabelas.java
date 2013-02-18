6d081c4169d13168b7e7b96b812b41baa3e581c6
marlonfa <marlonfa@gmail.com>
2013-02-18 16:17:59 -0300

    #31 Inserção do código para criação das tabelas Cliente e Endereco

--- Imobiliaria/src/conexaoDataBase/Tabelas.java
+++ Imobiliaria/src/conexaoDataBase/Tabelas.java
@@ -11,9 +11,24 @@ public class Tabelas {
     
     public void criarTabelas(){
         try{
-            st = connection.createStatement();           
-            String tabelaImovel = "CREATE TABLE IF NOT EXISTS imovel("
-                                + "id LONG AUTO_INCREMENT, "
+            st = connection.createStatement(); 
+            String tabelaCliente = "CREATE TABLE IF NOT EXISTS Cliente("
+                                + "id INT AUTO_INCREMENT, "
+                                + "nome VARCHAR(60) NOT NULL, "
+                                + "cpf VARCHAR(11) NOT NULL UNIQUE, "
+                                + "rg VARCHAR(10) NOT NULL UNIQUE, "
+                                + "data_nascimento DATE, "
+                                + "telefone VARCHAR(10), "
+                                + "celular VARCHAR(11), "
+                                + "email VARCHAR(30), "
+                                + "estado_civil VARCHAR(14), "
+                                + "PRIMARY KEY(id))"
+                                + "ENGINE = InnoDB;";
+            
+            st.executeUpdate(tabelaCliente);
+            
+            String tabelaImovel = "CREATE TABLE IF NOT EXISTS Imovel("
+                                + "id INT AUTO_INCREMENT, "
                                 + "cliente_nome VARCHAR(50) NOT NULL, "
                                 + "endereco VARCHAR(50) NOT NULL, "
                                 + "bairro VARCHAR(50) NOT NULL, "
@@ -28,9 +43,25 @@ public class Tabelas {
                                 + "valor FLOAT NOT NULL, "
                                 + "descricao VARCHAR(300), "
                                 + "PRIMARY KEY(id), "
-                                + "FOREIGN KEY(cliente_nome) REFERENCES cliente(nome));";
+                                + "FOREIGN KEY(cliente_nome) REFERENCES Cliente(nome)) "
+                                + "ENGINE = InnoDB;";
 
             st.executeUpdate(tabelaImovel);
+            
+            String tabelaEndereco = "CREATE TABLE IF NOT EXISTS Cliente("
+                                + "id INT AUTO_INCREMENT, "
+                                + "id_cliente INT NOT NULL, "
+                                + "cep VARCHAR(8) NOT NULL, "
+                                + "uf VARCHAR(2), "
+                                + "bairro VARCHAR(50) NOT NULL, "
+                                + "rua VARCHAR(30), "
+                                + "numero INT, "
+                                + "PRIMARY KEY(id), "
+                                + "FOREIGN KEY(id_cliente) REFERENCES Cliente(id)) "
+                                + "ENGINE = InnoDB;";
+            
+            st.executeUpdate(tabelaEndereco);
+       
             st.close();
         }catch(SQLException e){
             System.out.println(e.getMessage());
