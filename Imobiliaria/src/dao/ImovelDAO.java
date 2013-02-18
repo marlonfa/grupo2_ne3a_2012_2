690ac43953150da588e6dd52714aa791dbe05d9d
marlonfa <marlonfa@gmail.com>
2013-02-18 15:56:50 -0300

    #26 Inserção do setAutoCommit(false); ACID

--- Imobiliaria/src/dao/ImovelDAO.java
+++ Imobiliaria/src/dao/ImovelDAO.java
@@ -19,54 +19,72 @@ import java.util.List;
 import javax.swing.JOptionPane;
 
 public class ImovelDAO {
+
     private Conexao conexao;
     private Connection connection;
     private PreparedStatement pst;
     private Statement st;
-    private ResultSet rs;  
+    private ResultSet rs;
     private List<ImovelEntidade> lista;
-    
-    
+
     public ImovelDAO() {
         this.conexao = new Conexao();
         this.connection = conexao.getConnection();
-        
-    }
-    
-    
-    
-    public void adicionar(ImovelEntidade imovel){
-        try{
-                pst = connection.prepareStatement("INSERT INTO imovel VALUES (null, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");            
-                pst.setString(1, "cliente");
-                pst.setString(2, imovel.getImovelEspec().getEndereco());
-                pst.setString(3, imovel.getImovelEspec().getBairro());
-                pst.setString(4, imovel.getImovelEspec().getCidade());
-                pst.setString(5, imovel.getImovelEspec().getCep());
-                pst.setString(6, imovel.getUf().toString());
-                pst.setString(7, imovel.getTipo().toString());
-                pst.setString(8, imovel.getCategoria().toString());
-                pst.setString(9, imovel.getModalidade().toString());
-                pst.setString(10, imovel.getStatus().toString());
-                pst.setString(11, imovel.getChaves().toString());
-                pst.setFloat(12, imovel.getValor());
-                pst.setString(13, imovel.getImovelEspec().getDescricao());  
-                pst.execute();
 
-                imovel = new ImovelEntidade();
-                JOptionPane.showMessageDialog(null, "Imóvel Cadastrado com Sucesso!");                
-                pst.close();
-            }catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }   
     }
-    
-    public void alterar(ImovelEntidade imovel){
-        try{
+
+    public void adicionar(ImovelEntidade imovel) {
+        try {
+            connection.setAutoCommit(false);
+            pst = connection.prepareStatement("INSERT INTO imovel VALUES (null, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            //pst.setString(1, "cliente");
+            pst.setString(1, imovel.getImovelEspec().getEndereco());
+            pst.setString(2, imovel.getImovelEspec().getBairro());
+            pst.setString(3, imovel.getImovelEspec().getCidade());
+            pst.setString(4, imovel.getImovelEspec().getCep());
+            pst.setString(5, imovel.getUf().toString());
+            pst.setString(6, imovel.getTipo().toString());
+            pst.setString(7, imovel.getCategoria().toString());
+            pst.setString(8, imovel.getModalidade().toString());
+            pst.setString(9, imovel.getStatus().toString());
+            pst.setString(10, imovel.getChaves().toString());
+            pst.setFloat(11, imovel.getValor());
+            pst.setString(12, imovel.getImovelEspec().getDescricao());
+            pst.execute();
+
+            imovel = new ImovelEntidade();
+            JOptionPane.showMessageDialog(null, "Imóvel Cadastrado com Sucesso!");
+            connection.commit();
+        } catch (SQLException ex1) {
+            if (connection != null) {
+                try {
+                    try {
+                        connection.rollback();
+                    } catch (SQLException ex2) {
+                        JOptionPane.showMessageDialog(null, ex2.getMessage());
+                    }
+                    throw ex1;
+                } catch (SQLException ex3) {
+                    JOptionPane.showMessageDialog(null, ex3.getMessage());
+                }
+            }
+
+        } finally {
+            try {
+                pst.close();                
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, ex.getMessage());
+            }
+        }
+    }
+
+    public void alterar(ImovelEntidade imovel) {
+        try {
+            connection.setAutoCommit(false);
             String query = "UPDATE imovel SET endereco = ?, bairro = ?, "
-                        + "cidade = ?, cep = ?, uf = ?, tipo = ?, categoria = ?, "
-                        + "modalidade = ?, status = ?, chaves = ?, valor = ?, descricao = ? "
-                        + "WHERE id = ?";
+                    + "cidade = ?, cep = ?, uf = ?, tipo = ?, categoria = ?, "
+                    + "modalidade = ?, status = ?, chaves = ?, valor = ?, descricao = ? "
+                    + "WHERE id = ?";
             pst = connection.prepareStatement(query);
             pst.setString(1, imovel.getImovelEspec().getEndereco());
             pst.setString(2, imovel.getImovelEspec().getBairro());
@@ -84,51 +102,89 @@ public class ImovelDAO {
             pst.execute();
 
             JOptionPane.showMessageDialog(null, "Imóvel Alterado com Sucesso!");
-            pst.close();
-        }catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-        }   
-        
+            connection.commit();
+        } catch (SQLException ex1) {
+            if (connection != null) {
+                try {
+                    try {
+                        connection.rollback();
+                    } catch (SQLException ex2) {
+                        JOptionPane.showMessageDialog(null, ex2.getMessage());
+                    }
+                    throw ex1;
+                } catch (SQLException ex3) {
+                    JOptionPane.showMessageDialog(null, ex3.getMessage());
+                }
+            }
+
+        } finally {
+            try {
+                pst.close();
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, ex.getMessage());
+            }
+        }
+
     }
-    
-    public void remover(ImovelEntidade imovel){
-        try{
+
+    public void remover(ImovelEntidade imovel) {
+        try {
+            connection.setAutoCommit(false);
             String query = "DELETE FROM imovel WHERE id = ?";
-            
-            int i = JOptionPane.showConfirmDialog(null ,"Deseja Excluir este imóvel?",  
-            "Excluir Imóvel",JOptionPane.YES_NO_OPTION);  
-            if (i == JOptionPane.YES_OPTION ) { 
+
+            int i = JOptionPane.showConfirmDialog(null, "Deseja Excluir este imóvel?",
+                    "Excluir Imóvel", JOptionPane.YES_NO_OPTION);
+            if (i == JOptionPane.YES_OPTION) {
                 pst = connection.prepareStatement(query);
                 pst.setLong(1, imovel.getId());
                 pst.execute();
-            } else {  
-              JOptionPane.showMessageDialog(null, "Operação cancelada!");
-            }  
-            pst.close();
-        }catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-        }   
+            } else {
+                JOptionPane.showMessageDialog(null, "Operação cancelada!");
+            }
+            connection.commit();
+        } catch (SQLException ex1) {
+            if (connection != null) {
+                try {
+                    try {
+                        connection.rollback();
+                    } catch (SQLException ex2) {
+                        JOptionPane.showMessageDialog(null, ex2.getMessage());
+                    }
+                    throw ex1;
+                } catch (SQLException ex3) {
+                    JOptionPane.showMessageDialog(null, ex3.getMessage());
+                }
+            }
+
+        } finally {
+            try {
+                pst.close();
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, ex.getMessage());
+            }
+        }
     }
-    
-    public List<ImovelEntidade> consultarPorEndereco(String consulta){
-        try{
+
+    public List<ImovelEntidade> consultarPorEndereco(String consulta) {
+        try {
+            connection.setAutoCommit(false);
             pst = connection.prepareStatement("SELECT * FROM imovel WHERE endereco LIKE ?");
-            pst.setString(1, "%"+consulta+"%");
+            pst.setString(1, "%" + consulta + "%");
             pst.execute();
             rs = pst.executeQuery();
-            
-            this.lista = new ArrayList<ImovelEntidade>();         
-            
-            while (rs.next()){
+
+            this.lista = new ArrayList<ImovelEntidade>();
+
+            while (rs.next()) {
                 ImovelEntidade imovel = new ImovelEntidade();
                 imovel.setId(rs.getInt("id"));
                 //imovel.setCliente(rs.getString("cliente"));
                 imovel.setImovelEspec(new ImovelEspecEntidade(
-                        rs.getString("endereco"), 
-                        rs.getString("bairro"), 
-                        rs.getString("cidade"), 
-                        rs.getString("cep"), 
-                        rs.getString("descricao")));                    
+                        rs.getString("endereco"),
+                        rs.getString("bairro"),
+                        rs.getString("cidade"),
+                        rs.getString("cep"),
+                        rs.getString("descricao")));
                 imovel.setCategoria((ImovelCategoriaEntidade.valueOf(rs.getString("categoria"))));
                 imovel.setModalidade((ImovelModalidadeEntidade.valueOf(rs.getString("modalidade"))));
                 imovel.setStatus((ImovelStatusEntidade.valueOf(rs.getString("status"))));
@@ -136,36 +192,54 @@ public class ImovelDAO {
                 imovel.setValor(rs.getFloat("valor"));
                 imovel.setUf((EstadoEntidade.valueOf(rs.getString("uf"))));
                 imovel.setTipo((ImovelTipoEntidade.valueOf(rs.getString("tipo"))));
-                this.lista.add(imovel);    
+                this.lista.add(imovel);
             }
-            
-                pst.close();     
+            connection.commit();
+        } catch (SQLException ex1) {
+            if (connection != null) {
+                try {
+                    try {
+                        connection.rollback();
+                    } catch (SQLException ex2) {
+                        JOptionPane.showMessageDialog(null, ex2.getMessage());
+                    }
+                    throw ex1;
+                } catch (SQLException ex3) {
+                    JOptionPane.showMessageDialog(null, ex3.getMessage());
+                }
+            }
+
+        } finally {
+            try {
+                pst.close();
                 rs.close();
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, ex.getMessage());
+            }
         }
-        catch(SQLException ex){
-            System.out.println(ex.getMessage());
-        }return this.lista;     
+        return this.lista;
     }
-    
-    public List<ImovelEntidade> consultarPorCidade(String consulta){
-        try{
+
+    public List<ImovelEntidade> consultarPorCidade(String consulta) {
+        try {
+            connection.setAutoCommit(false);
             pst = connection.prepareStatement("SELECT * FROM imovel WHERE cidade LIKE ?");
-            pst.setString(1, "%"+consulta+"%");
+            pst.setString(1, "%" + consulta + "%");
             pst.execute();
             rs = pst.executeQuery();
-            
-            this.lista = new ArrayList<ImovelEntidade>();         
-            
-            while (rs.next()){
+
+            this.lista = new ArrayList<ImovelEntidade>();
+
+            while (rs.next()) {
                 ImovelEntidade imovel = new ImovelEntidade();
                 imovel.setId(rs.getInt("id"));
                 //imovel.setCliente(rs.getString("cliente"));
                 imovel.setImovelEspec(new ImovelEspecEntidade(
-                        rs.getString("endereco"), 
-                        rs.getString("bairro"), 
-                        rs.getString("cidade"), 
-                        rs.getString("cep"), 
-                        rs.getString("descricao")));                    
+                        rs.getString("endereco"),
+                        rs.getString("bairro"),
+                        rs.getString("cidade"),
+                        rs.getString("cep"),
+                        rs.getString("descricao")));
                 imovel.setCategoria((ImovelCategoriaEntidade.valueOf(rs.getString("categoria"))));
                 imovel.setModalidade((ImovelModalidadeEntidade.valueOf(rs.getString("modalidade"))));
                 imovel.setStatus((ImovelStatusEntidade.valueOf(rs.getString("status"))));
@@ -173,34 +247,53 @@ public class ImovelDAO {
                 imovel.setValor(rs.getFloat("valor"));
                 imovel.setUf((EstadoEntidade.valueOf(rs.getString("uf"))));
                 imovel.setTipo((ImovelTipoEntidade.valueOf(rs.getString("tipo"))));
-                this.lista.add(imovel);    
+                this.lista.add(imovel);
             }
-            
-                pst.close();     
+
+            connection.commit();
+        } catch (SQLException ex1) {
+            if (connection != null) {
+                try {
+                    try {
+                        connection.rollback();
+                    } catch (SQLException ex2) {
+                        JOptionPane.showMessageDialog(null, ex2.getMessage());
+                    }
+                    throw ex1;
+                } catch (SQLException ex3) {
+                    JOptionPane.showMessageDialog(null, ex3.getMessage());
+                }
+            }
+
+        } finally {
+            try {
+                pst.close();
                 rs.close();
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, ex.getMessage());
+            }
         }
-        catch(SQLException ex){
-            System.out.println(ex.getMessage());
-        }return this.lista;     
+        return this.lista;
     }
-    
-    public List<ImovelEntidade> todosRegistros(){
-        try{
+
+    public List<ImovelEntidade> todosRegistros() {
+        try {
+            connection.setAutoCommit(false);
             String query = "SELECT * FROM imovel";
             st = connection.createStatement();
             rs = st.executeQuery(query);
-            this.lista = new ArrayList<ImovelEntidade>();         
-            
-            while (rs.next()){
+            this.lista = new ArrayList<ImovelEntidade>();
+
+            while (rs.next()) {
                 ImovelEntidade imovel = new ImovelEntidade();
                 imovel.setId(rs.getInt("id"));
                 //imovel.setCliente(rs.getString("cliente"));
                 imovel.setImovelEspec(new ImovelEspecEntidade(
-                        rs.getString("endereco"), 
-                        rs.getString("bairro"), 
-                        rs.getString("cidade"), 
-                        rs.getString("cep"), 
-                        rs.getString("descricao")));                    
+                        rs.getString("endereco"),
+                        rs.getString("bairro"),
+                        rs.getString("cidade"),
+                        rs.getString("cep"),
+                        rs.getString("descricao")));
                 imovel.setCategoria((ImovelCategoriaEntidade.valueOf(rs.getString("categoria"))));
                 imovel.setModalidade((ImovelModalidadeEntidade.valueOf(rs.getString("modalidade"))));
                 imovel.setStatus((ImovelStatusEntidade.valueOf(rs.getString("status"))));
@@ -208,17 +301,34 @@ public class ImovelDAO {
                 imovel.setValor(rs.getFloat("valor"));
                 imovel.setUf((EstadoEntidade.valueOf(rs.getString("uf"))));
                 imovel.setTipo((ImovelTipoEntidade.valueOf(rs.getString("tipo"))));
-                this.lista.add(imovel);    
+                this.lista.add(imovel);
             }
-                rs.close();
-                st.close();     
-                
-        }
-        catch(SQLException ex){
-            System.out.println(ex.getMessage());
-        }return this.lista;             }
+            connection.commit();
+        } catch (SQLException ex1) {
+            if (connection != null) {
+                try {
+                    try {
+                        connection.rollback();
+                    } catch (SQLException ex2) {
+                        JOptionPane.showMessageDialog(null, ex2.getMessage());
+                    }
+                    throw ex1;
+                } catch (SQLException ex3) {
+                    JOptionPane.showMessageDialog(null, ex3.getMessage());
+                }
+            }
 
-    public List<ImovelEntidade> getLista(){
+        } finally {
+            try {       
+                rs.close();
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, ex.getMessage());
+            }
+        }
+        return this.lista;
+    }
+
+    public List<ImovelEntidade> getLista() {
         return this.lista;
     }
 }
