9b7ca5ac58700cb6993ebaacd4f3ff1e1e6407ae
marlonfa <marlonfa@gmail.com>
2013-04-15 16:24:55 -0300

    Atualização da classe MaskUtil

--- Imobiliaria/src/util/MaskUtil.java
+++ Imobiliaria/src/util/MaskUtil.java
@@ -5,12 +5,13 @@
 package util;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import javax.swing.JFormattedTextField;
 import javax.swing.text.MaskFormatter;
 
 /**
  *
- * @author marlon
+ * @author marlon e eder
  */
 public class MaskUtil {
     
@@ -23,10 +24,50 @@ public class MaskUtil {
         mask.install(textfield);  
         return mask;  
     }
-    
+
     public MaskFormatter maskCpf(JFormattedTextField textfield) throws ParseException{
         MaskFormatter mask = null;  
         mask = new MaskFormatter("###.###.###-##");  
+        mask.setOverwriteMode(true);  
+        mask.setValidCharacters("0123456789");  
+        mask.setPlaceholderCharacter('_');  
+        mask.install(textfield);  
+        return mask;  
+    }
+    
+    public MaskFormatter maskRg(JFormattedTextField textfield) throws ParseException{
+        MaskFormatter mask = null;  
+        mask = new MaskFormatter("##.###.###-#");  
+        mask.setOverwriteMode(true);  
+        mask.setValidCharacters("0123456789");  
+        mask.setPlaceholderCharacter('_');  
+        mask.install(textfield);  
+        return mask;  
+    }
+    
+    public MaskFormatter maskCelular(JFormattedTextField textfield) throws ParseException{
+        MaskFormatter mask = null;  
+        mask = new MaskFormatter("(##)#####-####");  
+        mask.setOverwriteMode(true);  
+        mask.setValidCharacters("0123456789");  
+        mask.setPlaceholderCharacter('_');  
+        mask.install(textfield);  
+        return mask;  
+    }
+    
+    public MaskFormatter maskTelFixo(JFormattedTextField textfield) throws ParseException{
+        MaskFormatter mask = null;  
+        mask = new MaskFormatter("(##)####-####");  
+        mask.setOverwriteMode(true);  
+        mask.setValidCharacters("0123456789");  
+        mask.setPlaceholderCharacter('_');  
+        mask.install(textfield);  
+        return mask;  
+    }
+    
+    public MaskFormatter maskCep(JFormattedTextField textfield) throws ParseException{
+        MaskFormatter mask = null;  
+        mask = new MaskFormatter("#####-###");  
         mask.setOverwriteMode(true);  
         mask.setValidCharacters("0123456789");  
         mask.setPlaceholderCharacter('_');  
