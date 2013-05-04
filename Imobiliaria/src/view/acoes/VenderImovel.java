93f159e78c8eb0e05f477c85009a9223f39c1ec0
eder92esf <eder.esf92@gmail.com>
2013-05-03 22:42:20 -0300

    #49 Atualização para gravar no banco no do proprietario

--- Imobiliaria/src/view/acoes/VenderImovel.java
+++ Imobiliaria/src/view/acoes/VenderImovel.java
@@ -11,6 +11,7 @@ import java.awt.Frame;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.swing.JOptionPane;
+import model.cliente.ClienteEntity;
 import model.imovel.ImovelVendaEntity;
 import org.hibernate.LockOptions;
 import org.hibernate.Session;
@@ -55,6 +56,11 @@ public class VenderImovel extends javax.swing.JDialog {
             jTFLocadorNome.setText(ImovelController.getImovelSelecionado().getCliente().getNome());
             jTFLocadorCPF.setText(ImovelController.getImovelSelecionado().getCliente().getCpf());
         }        
+    }
+    
+    private ClienteEntity getProprietario(){
+        ClienteController.setClienteSelecionado(ImovelController.getImovelSelecionado().getCliente());
+        return ClienteController.getClienteSelecionado();
     }
 
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -327,6 +333,7 @@ public class VenderImovel extends javax.swing.JDialog {
         venda.setDataContrato(new Date());
         venda.setImovel(ImovelController.getImovelSelecionado());
         venda.setLocatario(ClienteController.getClienteSelecionado());
+        venda.setProprietario(getProprietario());
         int opcao = JOptionPane.showConfirmDialog(null, "Deseja Vender este Imóvel? " , "Alugar Imóvel", 2);
             if(opcao == 0){
                 try{
@@ -340,6 +347,7 @@ public class VenderImovel extends javax.swing.JDialog {
     }//GEN-LAST:event_jBVenderActionPerformed
 
     private void jBFecharActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBFecharActionPerformed
+        this.session.close();
         dispose();
     }//GEN-LAST:event_jBFecharActionPerformed
 
